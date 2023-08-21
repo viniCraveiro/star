3a6b3201bd8e172ba33d5f4f874cb2f577f0a73e
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2023-08-21 20:03:36 -0300

    Initial Project

---
+++ src/main/java/com/fravel/star/SistemaDeTransparenciaEAcessoRapidoApplication.java
@@ -0,0 +1,13 @@
+package com.fravel.star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeTransparenciaEAcessoRapidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeTransparenciaEAcessoRapidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fravel/star/SistemaDeTransparenciaEAcessoRapidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fravel.star;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeTransparenciaEAcessoRapidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
