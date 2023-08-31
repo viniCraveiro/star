f98a5068952920433dc3df4642997a7b74f85a44
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2023-08-31 20:26:17 -0300

    first commit

---
+++ src/main/java/com/fravel/star/domain/Entidade.java
@@ -0,0 +1,20 @@
+package com.fravel.star.domain;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class Entidade implements IEntidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    protected  Entidade(){}
+
+    protected Entidade(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/fravel/star/domain/IEntidade.java
@@ -0,0 +1,10 @@
+package com.fravel.star.domain;
+
+import java.io.Serializable;
+
+public interface IEntidade extends Serializable {
+
+    Long getId();
+
+
+}
